5aa41b16284c1fb5d075e01cfbc10e20dfcb81e3
Ermi <manermidem@gmail.com>
2022-12-21 11:10:42 +0300

    Import react and add some code

---
+++ src/pages/quotePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Quote() {
+  return (
+    <h3>
+      "Mathematics is not about numbers, equations, computations, or algorithms:
+      it is about understanding." - William Paul Thurstone
+    </h3>
+  );
+}
+
+export default Quote;
