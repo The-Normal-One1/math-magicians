c8db08f27af08fdfd978ca4781052bf092fecbab
Ermi <manermidem@gmail.com>
2022-12-21 11:00:06 +0300

    Add some code into homePage.js file

---
+++ src/pages/homePage.js
@@ -0,0 +1,25 @@
+// /* eslint-disable max-len */
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="home">
+      <h2>Welcome to our page!</h2>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi nam
+        cumque similique quidem nemo alias iusto officia, unde doloribus iste
+        suscipit vel omnis quis fugiat provident quibusdam nobis rem dolor
+        assumenda aut? Sed ipsa nam molestiae molestias saepe inventore! Velit!
+      </p>
+
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi nam
+        cumque similique quidem nemo alias iusto officia, unde doloribus iste
+        suscipit vel omnis quis fugiat provident quibusdam nobis rem dolor
+        assumenda aut? Sed ipsa nam molestiae molestias saepe inventore! Velit!
+      </p>
+    </div>
+  );
+}
+
+export default Home;
