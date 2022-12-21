ac96ec0e449287ce8b90ade49a70e34c87543550
Ermi <manermidem@gmail.com>
2022-12-21 13:56:03 +0300

    Import Navbar and routers

--- src/App.js
+++ src/App.js
@@ -4,15 +4,26 @@ import './App.css';
 import Home from './pages/homePage';
 import Math from './pages/calculatorPage';
 import Quote from './pages/quotePage';
+import NavBar from './components/navBar/navBar';
+import { Route, Routes } from 'react-router-dom';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Math />
-        <Home />
-        <Quote />
+        <NavBar />
+        <Routes>
+          <Route>
+            <Math />
+          </Route>
+          <Route>
+            <Home />
+          </Route>
+          <Route>
+            <Quote />
+          </Route>
+        </Routes>
       </div>
     );
   }
