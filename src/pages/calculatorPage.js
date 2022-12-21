7e539ccbd45567443d9cf61c8a84b49333e5b85b
Ermi <manermidem@gmail.com>
2022-12-21 11:04:00 +0300

    Import Calculator.js file

---
+++ src/pages/calculatorPage.js
@@ -0,0 +1,12 @@
+import Calculator from '../components/Calculator';
+
+const Math = () => {
+  return (
+    <div>
+      <h3>Let's do some math!</h3>
+      <Calculator />
+    </div>
+  );
+};
+
+export default Math;
