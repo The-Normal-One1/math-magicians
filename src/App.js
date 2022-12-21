e53ce60209bbd6afcbd904c70442c1b5bc9154ac
Ermi <manermidem@gmail.com>
2022-12-21 13:37:13 +0300

    Import Home, Math, and Quote files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
-import Calculator from './components/Calculator';
+// import Calculator from './components/Calculator';
+import Home from './pages/homePage';
+import Math from './pages/calculatorPage';
+import Quote from './pages/quotePage';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Calculator />
+        <Math />
+        <Home />
+        <Quote />
       </div>
     );
   }
