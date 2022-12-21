72ebe7e1c89e7c80462de7a995cc6c4ff484e9cf
Ermi <manermidem@gmail.com>
2022-12-21 15:21:54 +0300

    Add class name

--- src/pages/calculatorPage.js
+++ src/pages/calculatorPage.js
@@ -5,7 +5,7 @@ import '../components/Calculator.css';
 function Math() {
   return (
     <div className="Calculate">
-      <h3>Let&apos;s do some math!</h3>
+      <h3 className="mathHeader">Let&apos;s do some math!</h3>
       <div className="App">
         <Calculator />
       </div>
--- src/pages/quotePage.js
+++ src/pages/quotePage.js
@@ -3,7 +3,7 @@ import '../components/Calculator.css';
 
 function Quote() {
   return (
-    <h3>
+    <h3 className="quoteHeader">
       &apos;Mathematics is not about numbers, equations, computations, or
       algorithms: it is about understanding.&apos; - William Paul Thurstone
     </h3>
